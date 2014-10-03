3aaabbe91bcf5c8b4e3d474ce38ad5dbe97f85c1
Katie Han <katie_han@brown.edu>
2014-10-02 23:22:36 -0400

    Initial Commit

---
+++ InteractiveProject/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  InteractiveProject
+//
+//  Created by Katie Han on 10/2/14.
+//  Copyright (c) 2014 Katie Han. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
